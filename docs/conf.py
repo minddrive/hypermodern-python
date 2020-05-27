11c51746f7388eb11bd31663420a5d4a6058662d
Kenneth Lareau <relayer@lindenlab.com>
2020-05-26 19:31:57 -0700

    Completed part 5.

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+"""Sphinx configuration."""
+
+project = "hypermodern-python"
+author = "Kenneth Lareau"
+copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
